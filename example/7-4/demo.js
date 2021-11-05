e8a5dcdef7a01608e46cc37b34f08e98f0cb44f0
annnn9Lee <angellee0924@gmail.com>
2021-11-05 09:14:39 +0800

    Create demo.js

---
+++ example/7-4/demo.js
@@ -0,0 +1,25 @@
+var math1 = 2 + 5; // Addition
+var math2 = 3 & 4.5; // *Multiplying
+var math3 = 1 - 5; // - Substracting
+var math4 = 20 / 5; // Dividing
+
+var math5 = 1 - 5 * 20; //Mutiple first
+//console.log(math5);
+
+var calc = math4 + math5; // Add math4 to math5 and store the result in the variable 'clac'
+//console.log(calc);
+
+var msg = 'The result of calculation is: ' + calc; // Conbine a numeric value with a string as a string
+//console.log(msg);
+
+var notANumber = '0' + '100' // These are strings, not numbers!
+//console.log(notANumber);
+
+// Mixing math calcuatio s with string combination
+var test1 = 'Result: ' + 5 + 10;
+var test2 = 'Result: ' + 5 * 10;
+//console.log(test1);
+//console.log(test2);
+
+var newString = 'I do not need the word DUMB.' - 'DUMB';
+//console.log(newString); // NaN = Not a Number 
