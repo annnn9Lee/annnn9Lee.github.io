d93f24416ae4de8120a81b758fed3678ba3c66e2
annnn9Lee <angellee0924@gmail.com>
2021-11-05 09:14:43 +0800

    Create demo.js

---
+++ example/7-3/demo.js
@@ -0,0 +1,35 @@
+// Single-line comment
+
+/* Mutiple-line comment:
+	When you have lots to say, you need this.
+*/
+
+// A "statement" ends with a semi-colon
+//console.log('This is an embedded script.');
+
+// A "code block" is defined with a pair of curly brackets.
+{
+	//console.log('This message is from a code block.');
+}
+
+// Variable
+var emptyVar; // A statement that creats a variable called "emptyVar"
+//console.log(emptyVar); // undefined
+
+var num = 5; //A statement that creats a variable called "num" with a defualt numeric value
+//console,log(num);
+
+var x = 10, y = 20, z = 40; //Create multiple variable in one single statement
+
+//var x = 20; //Potential Error
+
+x = 25; // Change the value of a variable
+//console.log(x);
+
+var name = 'Mike'; //Create a variable with a string as its default value
+var anotherName = "John"; // A pair of double-quotation marks also work for a string
+//var badQuotation = 'Ah" // Different quotation marks cause errors
+//console.log(anothername); //Error: Everything is case-sensitive in JavaScript
+
+x = 'This is a variable called x'; //Change the value to a different type of data; not recommened
+//console.log(x);
